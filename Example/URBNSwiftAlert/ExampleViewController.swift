7fec014fdb8755fc1e63075a8565d1d1e05ea1ef
Kevin Taniguchi <ktaniguchi@urbn.com>
2017-05-23 22:33:07 -0500

    UOA15-3680: URBNSwift Alert
    add basic example

--- Example/URBNSwiftAlert/ExampleViewController.swift
+++ Example/URBNSwiftAlert/ExampleViewController.swift
@@ -7,18 +7,28 @@
 //
 
 import UIKit
+import URBNConvenience
+import URBNSwiftAlert
 
 class ExampleViewController: UIViewController {
-
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        view.backgroundColor = .white
+        
+        let activeAlertBtn = UIButton(type: .custom)
+        activeAlertBtn.setTitle("Active Alert", for: .normal)
+        activeAlertBtn.setTitleColor(.green, for: .normal)
+        activeAlertBtn.addTarget(self, action: #selector(showActiveAlertBtn), for: .touchUpInside)
+        
+        
+        let leftSV = UIStackView(arrangedSubviews: [activeAlertBtn])
+        
+        _ = leftSV.wrapInView(view)
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    func showActiveAlertBtn() {
+        let activeAlert = URBNSwAlertViewController(title: "The Title of my message can be up to 2 lines long. It wraps and centers.", message:"And the message that is a bunch of text that will turn scrollable once the text view runs out of space.\nAnd the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.  And the message that is a bunch of text. And the message that is a bunch of text. And the message that is a bunch of text.")
+        activeAlert.show()
     }
-
 }
-
