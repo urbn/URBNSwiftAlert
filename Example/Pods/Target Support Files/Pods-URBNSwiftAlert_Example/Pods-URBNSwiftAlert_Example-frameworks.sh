42c575c511ac77b8a33ba85e6dc49ccb8fe468da
Kevin Taniguchi <ktaniguchi@urbn.com>
2017-07-13 08:56:39 -0500

    install pod

--- Example/Pods/Target Support Files/Pods-URBNSwiftAlert_Example/Pods-URBNSwiftAlert_Example-frameworks.sh
+++ Example/Pods/Target Support Files/Pods-URBNSwiftAlert_Example/Pods-URBNSwiftAlert_Example-frameworks.sh
@@ -89,9 +89,11 @@ strip_invalid_archs() {
 
 
 if [[ "$CONFIGURATION" == "Debug" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/URBNConvenience/URBNConvenience.framework"
   install_framework "$BUILT_PRODUCTS_DIR/URBNSwiftAlert/URBNSwiftAlert.framework"
 fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
+  install_framework "$BUILT_PRODUCTS_DIR/URBNConvenience/URBNConvenience.framework"
   install_framework "$BUILT_PRODUCTS_DIR/URBNSwiftAlert/URBNSwiftAlert.framework"
 fi
 if [ "${COCOAPODS_PARALLEL_CODE_SIGN}" == "true" ]; then
